40013a55c577649dd797e7bc293c114141010d7e
a-jumani <arsalan.jumani@outlook.com>
2020-03-20 20:41:46 +0400

    feat: Enable creating S3 read-only access role

--- manage_redshift.py
+++ manage_redshift.py
@@ -1,6 +1,7 @@
 import argparse
 import boto3
 import configparser
+import json
 import textwrap
 
 
@@ -23,7 +24,17 @@ DWH_DB_USER = config.get("DWH", "DWH_DB_USER")
 DWH_DB_PASSWORD = config.get("DWH", "DWH_DB_PASSWORD")
 DWH_PORT = config.get("DWH", "DWH_PORT")
 
+# IAM role name for Redshift cluster
+DWH_IAM_ROLE_NAME = config.get("DWH", "DWH_IAM_ROLE_NAME")
+
 # create relevant clients
+iam = boto3.client(
+    "iam",
+    region_name="us-west-2",
+    aws_access_key_id=KEY,
+    aws_secret_access_key=SECRET
+)
+
 redshift = boto3.client(
     "redshift",
     region_name="us-west-2",
@@ -52,6 +63,42 @@ def status():
             print(f"{k:20} {myClusterProps[k]}")
         except KeyError:
             pass
+
+
+def addS3AccessRole():
+    """ Enable read-only access to S3 bucket for a Redshift cluster
+    using IAM role.
+    """
+    # create a new role
+    try:
+        print("Creating a new IAM Role...")
+        iam.create_role(
+            Path='/',
+            RoleName=DWH_IAM_ROLE_NAME,
+            Description="Allows Redshift clusters to access AWS services.",
+            AssumeRolePolicyDocument=json.dumps(
+                {
+                    "Statement": [
+                        {
+                            "Action": "sts:AssumeRole",
+                            "Effect": "Allow",
+                            "Principal": {"Service": "redshift.amazonaws.com"}
+                        }
+                    ],
+                    "Version": "2012-10-17"
+                }
+            )
+        )
+    except Exception as e:
+        print(e)
+        return
+
+    # attach S3 read-only access policy to the new role
+    print("Attaching S3 read-only access policy...")
+    iam.attach_role_policy(
+        RoleName=DWH_IAM_ROLE_NAME,
+        PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+    )["ResponseMetadata"]["HTTPStatusCode"]
 
 
 if __name__ == "__main__":
